7eb2a392d7533f269b0dc51b47fdd80fd9a05df2
Daniele Procida <daniele@vurt.org>
2017-07-07 11:31:18 +0100

    Improved configuration options and logic

--- aldryn_config.py
+++ aldryn_config.py
@@ -12,6 +12,26 @@ class Form(forms.BaseForm):
         initial=True,
     )
 
+    # The generally recommended position for the DebugToolbarMiddleware is immediately after the
+    # GZipMiddleware. See:
+    #
+    # https://django-debug-toolbar.readthedocs.io/en/stable/installation.html#middleware
+    # https://django-debug-toolbar.readthedocs.io/en/stable/tips.html#middleware-isn-t-working-correctly
+
+    POSITIONS = [
+        ["top", "As close to the top as possible (generally recommended)"],
+        ["end", "As close to the end as possible"],
+
+    ]
+
+    toolbar_middleware_position = forms.SelectField(
+        'DebugToolbarMiddleware position',
+        help_text="Position in the middleware list",
+        choices=POSITIONS,
+        initial="top",
+    )
+
+
     def _show_toolbar(self, request):
         return True
 
@@ -23,10 +43,22 @@ class Form(forms.BaseForm):
 
             settings["DEBUG_TOOLBAR_CONFIG"] = {"SHOW_TOOLBAR_CALLBACK": self._show_toolbar}
 
-            settings["MIDDLEWARE_CLASSES"].insert(
-                settings["MIDDLEWARE_CLASSES"].index("django.middleware.gzip.GZipMiddleware") + 1,
-                "debug_toolbar.middleware.DebugToolbarMiddleware"
-            )
+            middleware = settings["MIDDLEWARE_CLASSES"]
+            debug_toolbar_middleware = "debug_toolbar.middleware.DebugToolbarMiddleware"
+            debug_toolbar_middleware_position = data["toolbar_middleware_position"]
+
+            if "django.middleware.gzip.GZipMiddleware" in middleware and debug_toolbar_middleware_position == "top":
+
+                middleware.insert(
+                    middleware.index("django.middleware.gzip.GZipMiddleware") + 1,
+                    debug_toolbar_middleware
+                )
+
+            elif debug_toolbar_middleware_position == "end":
+                middleware.append(debug_toolbar_middleware)
+
+            else:
+                middleware.insert(0, debug_toolbar_middleware)
 
             settings['ADDON_URLS'].append('aldryn_django_debug_toolbar.urls')
 
