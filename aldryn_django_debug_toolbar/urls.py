944e6a8974bd10bfd8a69abda53b0054e223f5b9
Stuart Axon <stuaxo2@yahoo.com>
2020-01-17 12:41:29 +0000

    Only modify urls if running as DEBUG and not during unit tests.
    Only modify urls DEBUG is True:
    Should be obvious why this is a good idea.
    
    Disable during unit testing:
    Django debug toolbar and view based tests don't always work well together so disable under test.

--- aldryn_django_debug_toolbar/urls.py
+++ aldryn_django_debug_toolbar/urls.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
+from django.conf import settings
 from django.conf.urls import url, include
 
+import sys
 import debug_toolbar
 
-urlpatterns = [url(r'^__debug__/', include(debug_toolbar.urls))]
+if settings.DEBUG and 'test' not in sys.argv:
+    urlpatterns = [url(r'^__debug__/', include(debug_toolbar.urls))]
